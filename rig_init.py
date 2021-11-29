898fd9c4cfbe98c4e147a30854e80d91c13da89b
Shenghua Su <shenghua.su@gmail.com>
2021-11-29 20:35:49 +0800

    init service add read ps light

--- rig_init.py
+++ rig_init.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 import gpio_control as control
+import pwr_light_sensor as pwrlight
 import time
+
 
 # --- wait time
 WAIT_INTERVAL = 0.3
@@ -14,4 +16,7 @@ time.sleep(WAIT_INTERVAL)
 
 # --- init fan pwm
 PWM_INIT_PERCENT = 32
-control.set_pwm(PWM_INIT_PERCENT/100.0)
+control.set_pwm1(PWM_INIT_PERCENT/100.0)
+
+# --- read ps status: since initial read return invalid result
+pwrlight.read_light()
